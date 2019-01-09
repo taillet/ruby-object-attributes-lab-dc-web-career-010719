3e286f230aad087f3effcdf37af142b558c19fd2
Heloise Taillet <ht2394@columbia.edu>
2019-01-09 05:29:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,20 @@
 class Dog
- 
-  def initialize(name)
+
+   def name=(name)
     @name = name
   end
- 
-  def name
+
+   def name
     @name
   end
- 
-  def name=(new_name)
-    @name = new_name
-  end
- 
-   def initialize(breed)
+
+   def breed=(breed)
     @breed = breed
   end
- 
-  def breed
+
+   def breed
     @breed
   end
- 
-  def breed=(new_breed)
-    @breed = new_breed
-  end
-end
+
+
+ end
